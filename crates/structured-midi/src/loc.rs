f5da3872fdbd7b3262b2c27730ec217e2399e84d
F.X.P. <litran39@hotmail.com>
2026-06-21 16:18:53 +0800

    chore: removed legacy `structured-midi` crate.

--- crates/structured-midi/src/loc.rs
+++
@@ -1,8 +0,0 @@
-pub type Channel = u8;
-
-#[derive(Debug, PartialOrd, Ord, PartialEq, Eq, Clone, Copy)]
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
-pub struct MultiTrackLoc {
-    pub track: usize,
-    pub channel: Channel,
-}
